091bd58dda58a9b3d35a206c53564952b8910a21
Mike <michael.mbea@gmail.com>
2020-09-18 17:54:17 +0100

    Prints numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : Prints number from 00..99
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int a;
+int b;
+for (a = '0'; a <= '9'; ++a)
+{
+for (b = '0'; b <= '9'; ++b)
+{
+if (a != '0' || b != '0')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(a);
+putchar(b);
+}
+}
+putchar('\n');
+return (0);
+}
