0eae18d3cd34dd62d6db4f98692b5505971657c8
Mike <michael.mbea@gmail.com>
2020-09-22 11:48:28 +0100

    remove comment

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,12 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
