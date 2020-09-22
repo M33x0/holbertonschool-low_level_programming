cade0734752eb471aa48af491ca94878d921bdec
Mike <michael.mbea@gmail.com>
2020-09-22 13:35:48 +0100

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*print_last_digit - the sign number
+*Description : integer a,b,result
+*Return: value of last digit
+*/
+int add(int a, int b)
+{
+int result = a + b;
+return (result);
+}
