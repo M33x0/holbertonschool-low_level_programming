b4849b9426ba863c5653e1784fd319ef54105708
Mike <michael.mbea@gmail.com>
2020-09-22 12:25:59 +0100

    changed variable name

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,9 @@
  *
  * Return: 1 if lowercase else return 0 otherwise
  */
-int _islower(int character)
+int _islower(int c)
 {
-if (character >= 97 && character <= 122)
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
