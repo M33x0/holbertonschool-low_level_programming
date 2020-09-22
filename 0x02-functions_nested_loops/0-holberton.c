c7e1d4fd9640f40c6a15ec02af179500f72c8370
Mike <michael.mbea@gmail.com>
2020-09-22 10:18:48 +0100

    Prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description : prints Holberton using _putchar
+ * Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	char hol[] = "Holberton";
+	int i;
+
+	for (i = 0 ; i < hol[i] ; i++)
+	{
+		_putchar(hol[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
