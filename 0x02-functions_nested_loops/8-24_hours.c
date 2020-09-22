cf0b718d8f6f35d2a9022061bcfa9c586c055bbb
Mike <michael.mbea@gmail.com>
2020-09-22 15:38:54 +0100

    24 Chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*jack_bauer - 24hr Chrono
+*@void: shayy
+*Return: kifkif shayy
+*/
+
+void jack_bauer(void)
+{
+int hours, minutes, h1, h2, m1, m2;
+for (hours = 0; hours < 24; hours++)
+{
+h1 = hours / 10;
+h2 = hours % 10;
+for (minutes = 0; minutes < 60; minutes++)
+{
+m1 = minutes / 10;
+m2 = minutes % 10;
+_putchar(h1 + '0');
+_putchar(h2 + '0');
+_putchar(':');
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}
+}
+}
