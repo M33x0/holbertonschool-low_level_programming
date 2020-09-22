3a51b90c12dc19e151ec528642596a36ad0b4a9f
Mike <michael.mbea@gmail.com>
2020-09-22 12:22:42 +0100

    changed variable name

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * Description : Function that check lowercase character
  * Return: 1 if lowercase else return 0 otherwise
  * 9- Check if c is between 9 & 122 from ASCII code
- **/
+ */
 int _islower(int character)
 {
 if (character >= 97 && character <= 122)
