a05f89c46eb96af03ec58389efdf3f25f1d5c761
Mike <michael.mbea@gmail.com>
2020-09-22 21:19:32 +0100

    fix numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * main - prints the sum of all numbers under 1024
  * that are divisible by 3 or 5
@@ -8,15 +7,14 @@
  */
 int main(void)
 {
-	int i, sum;
-
-	for (i = 0; i < 1024; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			sum += i;
-		}
-	}
-	printf("%i\n", sum);
-	return (0);
+int i, sum;
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%i\n", sum);
+return (0);
 }
