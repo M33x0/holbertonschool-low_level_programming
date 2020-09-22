fceef8005cc541f8aa4a2383b01804f328271130
Mike <michael.mbea@gmail.com>
2020-09-22 21:06:36 +0100

    added some function prototype

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,25 +2,24 @@
 #include <stdio.h>
 /**
 *print_to_98 - 9 times table
+*@n: integer
 *Return: 0
 */
 void print_to_98(int n)
 {
-
-	int i;
-
-	if (n <= 98)
-	{
-		for (i = n; i < 98; i++)
-		{
-			printf("%i, ", i);
-		}
-	} else
-	{
-		for (i = n; i > 98; i--)
-		{
-			printf("%i, ", i);
-		}
-	}
-	printf("98\n");
+int i;
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+printf("%i, ", i);
+}
+} else
+{
+for (i = n; i > 98; i--)
+{
+printf("%i, ", i);
+}
+}
+printf("98\n");
 }
