ca510d8983a0f050c2a0e6ce33fd456c36092343
Mike <michael.mbea@gmail.com>
2020-09-22 10:35:19 +0100

    Prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description : prints Holberton using _putchar
+ * Return: Always 0 (Success)
+*
+*/
+void print_alphabet(void)
+{
+char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0 ; i < alpha[i] ; i++)
+{
+_putchar(alpha[i]);
+}
+_putchar('\n');
+}
