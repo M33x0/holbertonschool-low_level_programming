1da5e35157ebab8d2421b548769101da7094446f
Mike <michael.mbea@gmail.com>
2020-09-23 19:52:24 +0100

    first 50 Fibonacci num,starting with 1& 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints first 50 numbers of the fibonacci sequence
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+	long a, b, next;
+
+	a = 1;
+	b = 2;
+	n = 50;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 49)
+		{
+			printf("%lu", a);
+		} else
+		{
+			printf("%lu, ", b);
+			next = a + b;
+			f1 = f2;
+			f2 = next;
+		}
+	}
+	printf("\n");
+	return (0);
+}
