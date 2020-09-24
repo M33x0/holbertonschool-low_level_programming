80e4ac454ead45918c6309a2d819991375131090
Mike <michael.mbea@gmail.com>
2020-09-24 12:11:59 +0100

    added holberton header

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/*0-isupper.c */
+int _isupper(int c);
+
+/*1-isdigit.c */
+int _isdigit(int c);
+
+/*2-mul.c*/
+int mul(int a, int b);
+
+/*3-print_numbers.c*/
+void print_numbers(void);
+
+/*4-print_most_numbers.c*/
+void print_most_numbers(void);
+
+/*5-more_numbers.c*/
+void more_numbers(void);
+
+/*6-print_line.c*/
+void print_line(int n);
+#endif
