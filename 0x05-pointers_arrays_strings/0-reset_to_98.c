a781d405487a18d6f6023fb90c0a1e85f4e1c4e1
Mike <michael.mbea@gmail.com>
2020-09-28 13:22:43 +0100

    update value with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+*reset_to_98 - the sign number
+*@n: integer pointer
+*Return: void
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/*0-reset_to_98.c*/
+void reset_to_98(int *n);
+#endif
