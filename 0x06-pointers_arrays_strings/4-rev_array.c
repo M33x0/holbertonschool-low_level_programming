c37732a4ccb58dabd1850e43ad9bd339a64a4124
Mike <michael.mbea@gmail.com>
2020-10-01 22:06:50 +0100

    added some space between operator

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
+
 /**
  * reverse_array - reverse the content of an array of intergers
- *
  * @a: pointer to integer
  * @n: integer
  */
@@ -9,7 +9,7 @@ void reverse_array(int *a, int n)
 {
 int i, j, swap;
 
-for ((i = 0, j = n - 1); i >= j; i++, j--)
+for ((i = 0, j = n - 1); i < j; i++, j--)
 {
 swap = a[i];
 a[i] = a[j];
