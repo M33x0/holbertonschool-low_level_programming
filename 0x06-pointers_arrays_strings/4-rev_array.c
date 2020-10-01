8ed83723ba3ea017352a44a062f3976760310794
Mike <michael.mbea@gmail.com>
2020-10-01 21:57:46 +0100

    reverse a array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverse the content of an array of intergers
+ *
+ * @a: pointer to integer
+ * @n: integer
+ */
+void reverse_array(int *a, int n)
+{
+int i, j, swap;
+
+for ((i = 0, j = n - 1); i< j; i++, j--)
+{
+swap = a[i];
+a[i] = a[j];
+a[j] = swap;
+}
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -8,4 +8,7 @@ char *_strcat(char *dest, char *src);
 
 /*3-strcmp.c*/
 int _strcmp(char *s1, char *s2);
+
+/*4-rev_array.c*/
+void reverse_array(int *a, int n);
 #endif
