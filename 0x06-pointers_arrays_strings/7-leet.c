74a3d722c58e29eb84c8e3b86279fae38cc45305
Mike <michael.mbea@gmail.com>
2020-10-01 23:47:51 +0100

    encode a string to 1337,added space between ',' 11

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 /**
  * *leet - encodes a string into 1337
- *
- * @c: char 
- *
+ * @c: char
  * Return: c
  */
 char *leet(char *c)
 {
 int i, j;
 char lower_upper[] = "aeotlAEOTL";
-char numbers[] = {'4', '3', '0', '7', '1','4', '3', '0', '7', '1'};
+char numbers[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 for (i = 0; c[i] != '\0'; i++)
 for (j = 0; lower_upper[j] != '\0'; j++)
 if (c[i] == lower_upper[j])
