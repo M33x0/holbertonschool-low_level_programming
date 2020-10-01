f0424703734da5fb7f77e94c7574b5f85e2f5af6
Mike <michael.mbea@gmail.com>
2020-10-01 12:32:42 +0100

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+
+/*0-strcat.c*/
+char *_strcat(char *dest, char *src);
+
+/*3-strcmp.c*/
+int _strcmp(char *s1, char *s2);
+#endif
