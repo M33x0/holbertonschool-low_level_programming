103525bff42eab8bad9019226b744b5dc21cacbc
Mike <michael.mbea@gmail.com>
2020-10-05 22:44:28 +0100

    Locate character in a given string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,4 @@
 #include "holberton.h"
-
-/**
- * _strchr - locate a character in a string
- * @s: string
- * @c: character to locate
- * Return: pointer s
- */
-#include "holberton.h"
 /**
 *  *_strchr - function that locates a character in a string.
 * @s: string
