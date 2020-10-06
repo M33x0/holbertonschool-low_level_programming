73acb071b817d5c765fec85eaed712ac71b4da2c
Mike <michael.mbea@gmail.com>
2020-10-06 15:59:40 +0100

    locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strstr - find the first occurrence
+ * @haystack: string
+ * @needle: substring
+ * Return: a pointer to needle or NULL if no byte is found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != 0)
+	{
+		char *start = haystack;
+		char *sub = needle;
+
+		while (*haystack == *sub && *sub != 0 && *haystack != 0)
+			haystack++, sub++;
+		if (*sub == 0)
+			return (start);
+		haystack = ++start;
+	}
+	return (0);
+}
