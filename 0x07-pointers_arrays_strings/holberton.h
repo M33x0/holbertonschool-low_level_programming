14a4d6bb056f25311046101f5fa857fa21521e5b
Mike <michael.mbea@gmail.com>
2020-10-06 15:11:07 +0100

    sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -18,6 +18,12 @@ unsigned int _strspn(char *s, char *accept);
 /*4-strpbrk.c*/
 char *_strpbrk(char *s, char *accept);
 
-/**/
+/*5-strstr.c*/
+char *_strstr(char *haystack, char *needle);
+
+/*7-print_chessboard.c*/
 void print_chessboard(char (*a)[8]);
+
+/*9-set_string.c*/
+void set_string(char **s, char *to);
 #endif
