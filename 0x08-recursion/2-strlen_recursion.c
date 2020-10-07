eb5b120f783ec45b8b5b1bedeed53acf845bbdf0
Mike <michael.mbea@gmail.com>
2020-10-07 20:39:58 +0100

    added comment

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
 
+/**
+  * _strlen_recursion - Return the length of a string
+  * @s: the string to count
+  * Return: integer(length of a string)
+  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
