826aa173ea2dffecdbfea9af99d58fb19eab6f7c
Mike <michael.mbea@gmail.com>
2020-10-11 15:16:32 +0100

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 
 /**
-*main - printing the number of arguments passed
-*@argc: length of argv
-*@argv: array of strings
-*Return: 1
-*/
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
-int result;
+	int result;
+
 	if (argc != 3)
 	{
-		printf("Error!!\n");
+		printf("Error\n");
 		return (1);
 	}
+
 	result = atoi(argv[1]) * atoi(argv[2]);
 
 	printf("%d\n", result);
+
 	return (0);
 }
