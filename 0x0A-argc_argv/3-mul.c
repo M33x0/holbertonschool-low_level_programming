739e46e69453faace6649ebaf64acfba6849da83
Mike <michael.mbea@gmail.com>
2020-10-11 14:39:05 +0100

    muliplications of two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return:  0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	if (argc != 3)
+	{
+		printf("ERROR!!");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = a * b;
+		printf("%d\n", result);
+	}
+	return (0);	
+}
