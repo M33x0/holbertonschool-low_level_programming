f73a495fc0aa9b9b6fa4ad25c0b668cc56e9d1ef
Mike <michael.mbea@gmail.com>
2020-10-13 22:00:16 +0100

    returns a pointer to a new string duplicated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 
 /**
-*_strdup - returns a pointer to a newly allocated space 
+*_strdup - returns a pointer to a newly allocated space
 * @str: char ,string to copied
-* @c: char to initialize the array with
-* Return: a pointer to the array, or NULL if it fails
+* Return: a pointer to the duplicate string
 */
 
 char *_strdup(char *str)
 {
 	unsigned int i;
 	char *duplicate;
-	
+
 	duplicate = malloc(sizeof(char));
-	if(str == NULL || duplicate == NULL)
+	if (str == NULL || duplicate == NULL)
 	{
 		return (NULL);
 	}
