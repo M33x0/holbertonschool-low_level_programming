f66b65faf03b97587056401936610d3f57a41500
Mike <michael.mbea@gmail.com>
2020-10-13 18:56:09 +0100

    creates an array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+* create_array - changes lowercase letters to uppercase
+* @size: integer size of array to initialized
+* @c: char to initialize the array with
+* Return: a pointer to the array, or NULL if it fails
+*/
 char *create_array(unsigned int size, char c)
 {
 	unsigned int  i;
