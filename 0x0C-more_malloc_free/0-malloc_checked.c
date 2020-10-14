01cd2d87400a9c977977898d5635a46f39f444dc
Mike <michael.mbea@gmail.com>
2020-10-14 19:59:58 +0100

    fixed name

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of memory to be allocated
+ * Return: 98 if malloc fails, pointer to memory allocated otherwise
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory = NULL;
+
+	memory = malloc(sizeof(b));
+	if (memory == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (memory);
+	}
+}
