2a77937d466403df7cf43d4ccf89c8c43e1eac92
Mike <michael.mbea@gmail.com>
2020-10-14 15:43:56 +0100

    concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to find length
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+int i = 0;
+	while (*(s + i))
+	{
+		i++;
+	}
+return (i);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to new string consisting of concatenated strings
+ * or return NULL on failure
+ */
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int i, j, length1, length2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	concat = malloc(sizeof(char) * (length1  + length2) + 1);
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length1 ; i++)
+	{
+		concat[i] = s1[i];
+	}
+
+	for (j = 0; j < length2; j++, i++)
+	{
+		concat[i] = s2[j];
+	}
+	return (concat);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -8,4 +8,10 @@ char *create_array(unsigned int size, char c);
 
 /*1-strdup*/
 char *_strdup(char *str);
+
+/*2-str_concat.c*/
+char *str_concat(char *s1, char *s2);
+
+/*4-free_grid.c*/
+void free_grid(int **grid, int height);
 #endif
