4c24a3f853a82b18dc35992f578ddcb9120b0708
Mike <michael.mbea@gmail.com>
2020-10-19 14:13:54 +0100

    absolute value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_function_like_macro
-#define _3_function_like_macro
+#ifndef _FUNCTION_LIKE_MACRO
+#define _FUNCTION_LIKE_MACRO
 #define ABS(x) ((x < 0) ? -(x) : (x))
 #endif /*_3_function_like_macro*/
