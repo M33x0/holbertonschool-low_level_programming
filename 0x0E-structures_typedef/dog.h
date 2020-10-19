5ac6c0540dae7d0086b3cdab56fd50556f3377fd
Mike <michael.mbea@gmail.com>
2020-10-19 19:21:39 +0100

    Struct of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct of a dog
+ * @name: char *
+ * @age: integer
+ * @owner: char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
