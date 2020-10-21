2dee1e642c4cce21714c93f31ede5def3015cdba
Mike <michael.mbea@gmail.com>
2020-10-21 21:31:35 +0100

    searches integer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,7 @@ void print_name(char *, void (*)(char *));
 
 /*1-array_iterator.c*/
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/*2-int_index.c*/
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /* FUNCTION_POINTERS_H */
