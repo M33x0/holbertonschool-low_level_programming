b35412f98af63a4fb5f140bb0fb7ede05d2c3e4e
Mike <michael.mbea@gmail.com>
2020-10-21 20:07:22 +0100

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,13 +9,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-	{
-		name = "";
-	}
-	if (f == NULL)
-	{
-		return;
-	}
-	f(name);
+if (name == NULL)
+name = "";
+if (f == NULL)
+return;
+f(name);
 }
