63ca3cbf9f97fe4b149c0165072c69f8ed440d9c
Mike <michael.mbea@gmail.com>
2020-10-21 21:07:53 +0100

    iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,7 @@
 
 /*0-print_name.c*/
 void print_name(char *, void (*)(char *));
+
+/*1-array_iterator.c*/
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* FUNCTION_POINTERS_H */
