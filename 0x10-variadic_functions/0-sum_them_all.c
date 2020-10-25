2f42e3423cacf3e0a40cbb457b6f91abfb17deb9
Mike <michael.mbea@gmail.com>
2020-10-25 17:30:38 +0100

    fixed conditions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,12 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i, sum = 0;
 
-	va_start(numbers, n);
-
 	if (n == 0)
 	{
 		return (0);
 	}
+
+	va_start(numbers, n);
+
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(numbers, int);
