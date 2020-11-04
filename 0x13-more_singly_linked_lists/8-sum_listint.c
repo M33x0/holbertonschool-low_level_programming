0236b0b1b2ddee9efe06f04ad96c37aac56931a3
Mike <michael.mbea@gmail.com>
2020-11-04 19:05:44 +0100

    sum all data of the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *sum_listint - calculate the sum of all the data of the list
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
