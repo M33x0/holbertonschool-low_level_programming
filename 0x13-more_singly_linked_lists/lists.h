35a261904b4fdc3eb44faa359e933ab74f63447e
Mike <michael.mbea@gmail.com>
2020-11-04 12:36:24 +0100

    added prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,4 +20,6 @@ typedef struct listint_s
 int _putchar(char c);
 /*0-print_listint.c*/
 size_t print_listint(const listint_t *h);
+/*1-listint_len.c*/
+size_t listint_len(const listint_t *h);
 #endif /*_LISTS_H*/
