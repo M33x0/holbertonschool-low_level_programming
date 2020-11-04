f9621ce8f79395268514c510ca21cef261ecaf21
Mike <michael.mbea@gmail.com>
2020-11-04 19:26:14 +0100

    fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,16 +4,15 @@
  * sum_listint - calculate the sum of all the data of the list
  * @head: first node of the list
  * Return : sum of data of the list
-*/
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	/*
-	*if (head == NULL)
+
+	if (head == NULL)
 	{
 		return (0);
 	}
-	*/
 	while (head != NULL)
 	{
 		sum += head->n;
