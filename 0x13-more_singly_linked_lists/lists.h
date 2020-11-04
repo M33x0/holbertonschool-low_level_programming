38da9ac48a12aea44a86ff2dba2a230b5a5b4830
Mike <michael.mbea@gmail.com>
2020-11-04 13:17:32 +0100

    added prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,9 +19,9 @@ typedef struct listint_s
 
 int _putchar(char c);
 /*0-print_listint.c*/
-
 size_t print_listint(const listint_t *h);
 /*1-listint_len.c*/
-
 size_t listint_len(const listint_t *h);
+/*2-add_nodeint.c*/
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif /*_LISTS_H*/
