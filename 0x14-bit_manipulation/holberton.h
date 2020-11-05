9ce371fcd28234c2e5c0ca741ed476b5e2b85d5b
m33x0 <michael.mbea@gmail.com>
2020-11-05 23:15:44 +0100

    added prototype

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+/*0-binary_to_uint.c*/
+unsigned int binary_to_uint(const char *b);
+#endif /* _HOLBERTON_H*/
