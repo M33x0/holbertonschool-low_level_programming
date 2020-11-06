d2b108154b0af5788e7a116b135ebcfd99534c06
m33x0 <michael.mbea@gmail.com>
2020-11-06 18:03:30 +0100

    fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,9 +2,9 @@
 
 /**
  * delete_nodeint_at_index - Delete a node at a given position
- * @head: first node address.
- * @index: position of the node to delete.
- * Return: If success (1).
+ * @head: first node address
+ * @index: position of the node to delete
+ * Return: If success (1)
  **/
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
