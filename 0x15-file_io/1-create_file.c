daf8f3122d5a2027abc2427dc146e7cbb2794041
m33x0 <michael.mbea@gmail.com>
2020-11-09 22:35:19 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length];)
+		while (length != NULL)
+		{
 			length++;
+		}
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
