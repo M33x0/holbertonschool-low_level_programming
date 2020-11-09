1bae6477a758d1c0bf36ef4f228026c56649092f
m33x0 <michael.mbea@gmail.com>
2020-11-09 22:56:16 +0100

    append text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		while (text_content[length] != '\0')
+		{
+			length++;
+		}
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, length);
+
+	if (o == -1 || w == -1)
+	{
+		return (-1);
+		close(o);
+	}
+
+	return (1);
+}
