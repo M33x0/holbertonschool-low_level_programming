51ff4a7e0a89b85d192039406246998a45dbb473
m33x0 <michael.mbea@gmail.com>
2020-11-09 22:34:13 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int o, w;
 	int length = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
