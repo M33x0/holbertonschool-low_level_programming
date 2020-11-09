28dd90811b5cce48476ce044ff91c7fcc0edb988
m33x0 <michael.mbea@gmail.com>
2020-11-09 22:15:40 +0100

    addded prototype

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,7 +5,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 /*0-read_textfile.c*/
 ssize_t read_textfile(const char *filename, size_t letters);
+/*1-create_file.c*/
+int create_file(const char *filename, char *text_content);
 #endif /*_HOLBERTON_H*/
