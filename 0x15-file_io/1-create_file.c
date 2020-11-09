fd0bc7252e5c30dc74c7e46c6339d29345a1bbc3
m33x0 <michael.mbea@gmail.com>
2020-11-09 22:22:15 +0100

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * create_file - creates a file
+ * @filename: pointer to the name of the file to create
+ * @text_content: pointer to a string to write to the file
+ * Return: if the function fails - -1 Otherwise - 1
+ **/
 int create_file(const char *filename, char *text_content)
 {
 	int o, w;
