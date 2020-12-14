ce5f712ffda489b3876e200b0cc4ef5ce650d615
m33x0 <michael.mbea@gmail.com>
2020-12-14 14:30:16 +0100

    fixed indentation

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+  * print_dlistint - Print all elements of a list
+  * @h: Pointer to the first element in a list
+  * Return: The number of elements in the list
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *tmp;
+	size_t elements;
+	tmp = h;
+	elements = 0;
+		while (tmp != NULL)
+		{
+			printf("%d\n", tmp->n);
+			elements++;
+			tmp = tmp->next;
+		}
+	return (elements);
+}
