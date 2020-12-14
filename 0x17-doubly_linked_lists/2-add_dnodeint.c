b39dfd1ff7ce973e4d50f14d47c19d11d9cad770
m33x0 <michael.mbea@gmail.com>
2020-12-14 16:23:37 +0100

    fix betty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,11 +8,10 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node== NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -30,4 +29,3 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	*head = new_node;
 	return (new_node);
 }
-
