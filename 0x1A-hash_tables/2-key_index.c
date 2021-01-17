d2d60a5551c32f5079eeba0dfee86d1122c9c722
m33x0 <michael.mbea@gmail.com>
2021-01-17 22:56:33 +0100

    ffs

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,7 +2,7 @@
 
 /**
  * key_index - gives you the index of a key
- * @key: key
+ * @key: key to enter
  * @size: size of the array of the hash table
  *
  * Return: index at which the key/value pair should be stored
