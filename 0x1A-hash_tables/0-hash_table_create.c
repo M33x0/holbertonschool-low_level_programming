ac268e642d169ec0b8885172374dacdbc5e93552
m33x0 <michael.mbea@gmail.com>
2021-01-17 22:18:40 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i = 0;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
-	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t *));
-	if (new_table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 	{
 		return (NULL);
 	}
-	return (new_table);
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
 }
