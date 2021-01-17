cda7d5aa465b841ca7edd6f186d7838b604413a4
m33x0 <michael.mbea@gmail.com>
2021-01-17 22:42:15 +0100

    key->index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned int index = 0;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
