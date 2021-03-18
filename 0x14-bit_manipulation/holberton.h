d17b2829258beb25263b9774de142230caca1292
m33x0 <michael.mbea@gmail.com>
2021-03-18 23:10:41 +0100

    prototype

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -8,6 +8,8 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 /*1-print_binary.c*/
 void print_binary(unsigned long int n);
+int set_bit(unsigned long int *n, unsigned int index);
 /*100-get_endianness.c*/
 int get_endianness(void);
+int get_bit(unsigned long int n, unsigned int index);
 #endif /* _HOLBERTON_H*/
