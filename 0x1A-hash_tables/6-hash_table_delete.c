9344efde315aec548e005fc86b940a9c40256eef
m33x0 <michael.mbea@gmail.com>
2021-03-20 14:25:13 +0100

    delete hashT

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to a hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **array;
+	unsigned long int count = 0;
+	hash_node_t *current;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	array = ht->array;
+	while (count < ht->size)
+	{
+		current = array[count];
+		while (current != NULL)
+		{
+			tmp = current;
+			current = current->next;
+			free(tmp->value);
+			free(tmp->key);
+			free(tmp);
+		}
+		free(current);
+		count++;
+	}
+	free(array);
+	free(ht);
+}
