e522ce903e4c7bc94b130bd6e186c88ddd1c088b
M33x0 <michael.mbea@gmail.com>
2021-08-05 16:29:37 +0200

    concat

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,6 +6,9 @@ int _putchar(char c);
 /*0-malloc_checked.c*/
 void *malloc_checked(unsigned int b);
 
+/*1-string_nconcat.c*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 /*2-calloc.c*/
 void *_calloc(unsigned int nmemb, unsigned int size);
 
